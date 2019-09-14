588393f3f93873bc4a1ba3533f70111edf6ae91c
Tarun <tarunbhatt9784@gmail.com>
2019-09-14 10:21:56 +1000

    change contacts to users

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+import BootstrapTable from 'react-bootstrap-table-next';
+import 'bootstrap/dist/css/bootstrap.css';
+import cellEditFactory from 'react-bootstrap-table2-editor';
+const columns = [{
+    dataField: 'id',
+    text: 'Id'
+  }, {
+    dataField: 'name',
+    text: 'Name'
+  }, {
+    dataField: 'username',
+    text: 'User Name'
+  }, 
+  {
+    dataField: 'email',
+    text: 'Email',
+    validator: (newValue, row, column) => {
+      if (!newValue) {
+        return {
+          valid: false,
+          message: 'Email cannot be null'
+        };
+      }
+      if (!newValue.includes("@")) {
+        return {
+          valid: false,
+          message: 'Email Should have @ sign'
+        };
+      }
+      return true;
+    }
+  }];
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+export default class App extends Component{
+  render() {
+    return (
+        <div>
+        <BootstrapTable
+        keyField="id"
+        data={ this.state.users }
+        columns={ columns }
+        cellEdit={ cellEditFactory({
+          mode: 'click',
+          blurToSave: true
+        }) }
+      />
+
+
+        </div>)
 }
 
-export default App;
+state = {
+    users: []
+};
+
+componentDidMount() {
+    fetch('http://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then((data) => {
+            this.setState({ users: data })
+        })
+        .catch(console.log)
+}
+}
